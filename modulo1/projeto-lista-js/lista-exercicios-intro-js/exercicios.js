054cae8910c146301d84c995c7825d36e4fe4615
JULIANE BRITO <marcos-dido@hotmail.com>
2022-02-18 16:56:45 -0300

    projeto-lista-js

---
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -0,0 +1,145 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+// function soma(num1, num2) {
+//   // implemente sua lógica aqui
+//   return num1 + num2
+// }
+
+// // EXERCÍCIO 0B
+// function imprimeMensagem() {
+//   // implemente sua lógica aqui
+//   const mensagem = prompt('Digite uma mensagem!')
+
+//   console.log(mensagem)
+// }
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+
+
+
+// function calculaAreaRetangulo() {  
+  
+//   const mensagem = Number(prompt("Digite a Altura"))
+//   const mensagem2 = Number(prompt("Digite a largura"))
+//     const area = (mensagem * mensagem2)
+  
+//     console.log(area) 
+
+// }
+
+// // // EXERCÍCIO 02
+// function imprimeIdade() {
+//   const anoAtual = Number(prompt("Digite o ano atual"))
+//   const anoDeNascimento = Number(prompt("Digite o ano de seu nascimento"))
+//     const area = (anoAtual - anoDeNascimento)
+  
+//     console.log(area) 
+
+// }
+
+// // // EXERCÍCIO 03
+// function calculaIMC(peso, altura) {
+  
+//   return imc= peso / (altura*altura)  
+   
+// }
+
+// // EXERCÍCIO 04
+// function imprimeInformacoesUsuario() {
+
+//  const nome = prompt("Digite seu nome")
+//  const idade = prompt("Digite sua idade")
+//  const email = prompt("Digite seu email")
+
+//  console.log (`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+// }
+
+
+// // // EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  let cor1 = prompt("Digite três cores favoritas"); 
+  let cor2 = prompt("Digite três cores favoritas"); 
+  let cor3 = prompt("Digite três cores favoritas"); 
+console.log ([cor1, cor2, cor3]);
+  
+
+}
+
+// // EXERCÍCIO 06
+// function retornaStringEmMaiuscula(string) {
+//   return string.toUpperCase()
+// }
+
+// // EXERCÍCIO 07
+// function calculaIngressosEspetaculo(custo, valorIngresso) {
+//   const custoEspetaculo = Number (prompt("custo de um espetáculo de teatro"))
+//   const valorCadaIngresso = Number (prompt("valor de cada ingresso"))
+//   return (custo / valorIngresso) 
+
+// }
+
+// // EXERCÍCIO 08
+// function checaStringsMesmoTamanho(string1, string2) {
+//   const string0 = prompt("Digite uma string")
+//   const string3 = prompt("Digite uma string")
+//   return string1.length === string2.length
+
+// }
+
+// // EXERCÍCIO 09
+// function retornaPrimeiroElemento(array) {
+//     return array [0]
+
+// }
+
+// // EXERCÍCIO 10
+// function retornaUltimoElemento(array) {
+//     return array[array.length -1]
+
+// }
+
+// // EXERCÍCIO 11
+// function trocaPrimeiroEUltimo(array) {
+
+//   array.push(array.splice(2,1)[2]);
+
+//   return array.reverse();
+
+// }
+
+// // EXERCÍCIO 12
+// function checaIgualdadeDesconsiderandoCase(string1, string2) {
+//   const nome1 = prompt("Digite uma string")
+//   const nome2 = prompt("Digite uma string")
+//   return string1.toLowerCase() === string2.toLowerCase()
+
+// }
+
+// // EXERCÍCIO 13
+// function checaRenovacaoRG() {
+//   const anoAtual = prompt("Digite o Ano Atual")
+//   const anoDeNascimento = prompt("Digite o ano de seu nascimento")
+//   const anoDaCarteira = prompt("Digite o ano em que sua identidade foi emitida")
+
+//   const idade = 20;
+  
+//   const maior20 = idade >= 20;
+//   const maior202 = idade > 20 <= 50
+//   const maior50 = idade > 50
+
+// }
+
+// // EXERCÍCIO 14
+// function checaAnoBissexto(ano) {
+//   // implemente sua lógica aqui
+
+// }
+
+// // EXERCÍCIO 15
+// function checaValidadeInscricaoLabenu() {
+//   // implemente sua lógica aqui
+
+// }
